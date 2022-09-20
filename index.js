e100f31553f2d6eac2a4d7b5e7c945cb8084a4bd
Nawin <naveen.ravichandran@ysquaretechnology.com>
2022-09-20 16:17:39 +0530

    auto rotate

--- index.js
+++ index.js
@@ -85,7 +85,14 @@ function base64decode(str, decoder = new TextDecoder()) {
 
 
 
-
+    window.onorientationchange = function() { 
+        var orientation = window.orientation; 
+            switch(orientation) { 
+                case 0:
+                case 90:
+                case -90: window.location.reload(); 
+                break; } 
+    };
 
 
 window.addEventListener('load', function () {
